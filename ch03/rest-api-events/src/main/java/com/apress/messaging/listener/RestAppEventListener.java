48c434ce463d2ab00121d07ca100e7bd2b8e2eee
Felipe Gutierrez <fgutierrezcruz@pivotal.io>
2016-11-06 20:55:25 -0700

    updated

--- ch03/rest-api-events/src/main/java/com/apress/messaging/listener/RestAppEventListener.java
+++ ch03/rest-api-events/src/main/java/com/apress/messaging/listener/RestAppEventListener.java
@@ -9,6 +9,10 @@ import com.apress.messaging.annotation.Log;
 @Component
 public class RestAppEventListener {
 
+	//@EventListener(condition = "#springApp.args.length > 1")
+	//@EventListener({CurrencyEvent.class,CurrencyConversionEvent.class})
+	//@Order(Ordered.HIGHEST_PRECEDENCE)
+	//@Async
 	@EventListener
 	@Log(printParamsValues=true)
 	public void restAppHandler(SpringApplicationEvent springApp){
