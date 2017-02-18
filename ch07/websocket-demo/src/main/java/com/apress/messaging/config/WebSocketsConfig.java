ecfeeaef9ce260b1654560388a2669a6c8d5dc43
Felipe Gutierrez <fgutierrezcruz@pivotal.io>
2017-02-17 21:42:42 -0700

    adding comments and fixing some variables

--- ch07/websocket-demo/src/main/java/com/apress/messaging/config/WebSocketsConfig.java
+++ ch07/websocket-demo/src/main/java/com/apress/messaging/config/WebSocketsConfig.java
@@ -17,12 +17,23 @@ public class WebSocketsConfig extends AbstractWebSocketMessageBrokerConfigurer {
 	
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		//For the same domain
 		registry.addEndpoint(props.getEndpoint()).withSockJS();
+		
+		//Necessary if there is a cross-domain
+		//registry.addEndpoint(props.getEndpoint()).setAllowedOrigins("*").withSockJS();
 	}
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
-		config.enableSimpleBroker(props.getTopic());
+		//Application Prefix:
 		config.setApplicationDestinationPrefixes(props.getAppDestinationPrefix());
+		
+		//Enable the following for a simple broker
+		config.enableSimpleBroker(props.getTopic());
+		
+		//Enable the following for RabbitMQ Broker relay. If enable, then comment out the above statement:  enableSimpleBroker
+		//config.enableStompBrokerRelay("/topic", "/queue").setRelayPort(61613); // This is the RabbitMQ stomp port
+		
 	}
 }
