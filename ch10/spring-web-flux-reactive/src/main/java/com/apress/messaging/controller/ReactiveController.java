4d0e61427a3f0957e09c747f67a8ce7284be7c61
Felipe Gutierrez <fgutierrezcruz@pivotal.io>
2017-03-16 11:48:07 -0600

    updated

--- ch10/spring-web-flux-reactive/src/main/java/com/apress/messaging/controller/ReactiveController.java
+++ ch10/spring-web-flux-reactive/src/main/java/com/apress/messaging/controller/ReactiveController.java
@@ -2,6 +2,7 @@ package com.apress.messaging.controller;
 
 import org.springframework.http.MediaType;
 import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.SubscribableChannel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,18 +28,19 @@ public class ReactiveController {
 	@GetMapping(value="/person-watcher", produces=MediaType.TEXT_EVENT_STREAM_VALUE)
 	public Flux<Person> log4Person(){
 		return Flux.create( sink -> {
-			personChannel.subscribe(message -> sink.next((Person) message.getPayload()));
+			MessageHandler handler = message -> sink.next((Person) message.getPayload());
+			personChannel.subscribe(handler);
+			sink.setCancellation(() -> personChannel.unsubscribe(handler));
 		});
 				
 	}
 	
 	@PostMapping(value="/person", consumes = {MediaType.APPLICATION_JSON_VALUE})
 	public void createPerson(@RequestBody Person person){
-		repo.save(person);
-		personChannel.send(MessageBuilder.withPayload(person).build());
+		if(person != null && person.getName() != null){
+			repo.save(person);
+			personChannel.send(MessageBuilder.withPayload(person).build());
+		}
 	}
-	
-	
-	
 	
 }
